03b0ba473c3d57326ca81bf255f484400ab44d09
khanmdsohel02 <khanmdsohel02.25@gmail.com>
2022-05-05 04:39:36 +0600

    server connceted to db n load review data

--- index.js
+++ index.js
@@ -2,11 +2,37 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
+const { MongoClient, ServerApiVersion} = require('mongodb');
 require('dotenv').config();
 
 // middlerware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.knqju.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
+});
+ 
+async function run() {
+    
+    try {
+        await client.connect();
+        const dealerReviewCollection = client.db('dealerReview').collection('dealerReviewCollection');
+        app.get('/review', async  (req, res) => {
+            const query = {};
+            const cursor = dealerReviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews)
+     })
+        
+    }
+    finally{}
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('server side is running')
