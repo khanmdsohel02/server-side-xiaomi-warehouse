b3c921521172c0a777138f5cbb2f6124d957edee
khanmdsohel02 <khanmdsohel02.25@gmail.com>
2022-05-06 23:01:19 +0600

    product load using id, create api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
-const { MongoClient, ServerApiVersion} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 require('dotenv').config();
 
 // middlerware
@@ -30,13 +30,18 @@ async function run() {
         })
           await client.connect();
           const stockProductsCollections = client.db('stockProducts').collection('stockProductsCollection');
-          app.get('/products', async (req, res) => {
-              const query = {};
-              const cursor = stockProductsCollections.find(query);
-              const products = await cursor.toArray();
-              res.send(products)
-          })
-        
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = stockProductsCollections.find(query);
+            const products = await cursor.toArray();
+            res.send(products)
+        })
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const product = await stockProductsCollections.findOne(query);
+            res.send(product);
+         })
     }
     finally{}
 }
